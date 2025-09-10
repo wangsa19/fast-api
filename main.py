0182db55aba6fff1ebee092ba90d988b70e92b1b
wangsa <dewanggawpw@gmail.com>
2025-09-10 11:11:17 +0700

    feat: Completed Path Parameters

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from enum import Enum
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -13,6 +21,16 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+@app.get("/users")
+async def read_users():
+    return ["John", "Jane"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
@@ -21,3 +39,18 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
