f8fee292b4dd26e85a4ae25c7692427942864488
wangsa <dewanggawpw@gmail.com>
2025-09-13 12:36:20 +0700

    Body multiple parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import Annotated, Literal
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel, AfterValidator, Field
 
 
@@ -24,6 +24,11 @@ class FilterParams(BaseModel):
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
     tags: list[str] = []
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
 
 
 app = FastAPI()
@@ -177,6 +182,41 @@ async def create_item(item: Item):
 # @app.put("/items/{item_id}")
 # async def update_item(item_id: int, item: Item, q: str | None = None):
 #     return {"item_name": item.name, "item_id": item_id}
+
+
+# Body - multiple parameters
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+#     q: str | None = None,
+#     item: Item | None = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Item,
+#     user: User,
+#     importance: Annotated[int, Body()],
+#     q: str | None = None,
+# ):
+#     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
 
 
 # Path Parameters and Numeric Validations
